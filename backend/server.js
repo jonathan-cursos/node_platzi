230c6a634d522e0881b103c3b0c350d868a0f789
Jonathan García <jona03g97@gmail.com>
2021-04-16 05:00:46 -0600

    Recibir información desde el cliente: Body y Query

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,17 @@ let app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
 
-// app.use("/", (req, res) => {
-//   res.send("hola");
-// });
-
-app.use(router); //Añadir router a la app de express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
 
 router.get("/message", (req, res) => {
   res.send("Lista de mensajes");
 });
 
 router.post("/message", (req, res) => {
+  console.log(req.body);
+  console.log(req.query);
   res.send("Mensaje añadido");
 });
 
