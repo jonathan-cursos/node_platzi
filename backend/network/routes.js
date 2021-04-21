817671f786a0b8800ab4a36585251ce18245cdf2
Jonathan García <jona03g97@gmail.com>
2021-04-21 07:53:57 -0600

    Escalando la arquitectura: Múltiples entidades

--- backend/network/routes.js
+++ backend/network/routes.js
@@ -1,8 +1,9 @@
-//Para no declarar el router en cada archivo, y usar el mismo en todos, lo crearemos aquí
 const message = require("../components/message/network");
+const user = require("../components/user/network");
+
 const routes = function (server) {
-  //Usaremos el server para añadir todas las rutas
-  server.use("/message", message); //Cara vez que usemos la ruta /message, llamará al network del componente message
+  server.use("/message", message);
+  server.use("/user", user);
 };
 
 module.exports = routes;
